96b938ebb08897e3445c4a1d3a7afacfd53a5212
Summerfruity <2781455007@qq.com>
2025-08-09 17:59:08 +0800

    Initial commit

---
+++ src/build/CMakeFiles/render.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/summerfruity/tinyrenderer/src/geometry.cpp" "CMakeFiles/render.dir/geometry.cpp.o" "gcc" "CMakeFiles/render.dir/geometry.cpp.o.d"
+  "/home/summerfruity/tinyrenderer/src/main.cpp" "CMakeFiles/render.dir/main.cpp.o" "gcc" "CMakeFiles/render.dir/main.cpp.o.d"
+  "/home/summerfruity/tinyrenderer/src/model.cpp" "CMakeFiles/render.dir/model.cpp.o" "gcc" "CMakeFiles/render.dir/model.cpp.o.d"
+  "/home/summerfruity/tinyrenderer/src/our_gl.cpp" "CMakeFiles/render.dir/our_gl.cpp.o" "gcc" "CMakeFiles/render.dir/our_gl.cpp.o.d"
+  "/home/summerfruity/tinyrenderer/src/tgaimage.cpp" "CMakeFiles/render.dir/tgaimage.cpp.o" "gcc" "CMakeFiles/render.dir/tgaimage.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/CMakeFiles/render.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/render.dir/geometry.cpp.o"
+  "CMakeFiles/render.dir/geometry.cpp.o.d"
+  "CMakeFiles/render.dir/main.cpp.o"
+  "CMakeFiles/render.dir/main.cpp.o.d"
+  "CMakeFiles/render.dir/model.cpp.o"
+  "CMakeFiles/render.dir/model.cpp.o.d"
+  "CMakeFiles/render.dir/our_gl.cpp.o"
+  "CMakeFiles/render.dir/our_gl.cpp.o.d"
+  "CMakeFiles/render.dir/tgaimage.cpp.o"
+  "CMakeFiles/render.dir/tgaimage.cpp.o.d"
+  "render"
+  "render.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/render.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
